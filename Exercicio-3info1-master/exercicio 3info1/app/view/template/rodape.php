8de2ef93f5c8ff5dfe85cb4467b8df897705c88b
André Pacheco <anddreluiz1@gmail.com>
2018-04-10 08:02:33 -0300

    Update rodape.php

--- Exercicio-3info1-master/exercicio 3info1/app/view/template/rodape.php
+++ Exercicio-3info1-master/exercicio 3info1/app/view/template/rodape.php
@@ -3,7 +3,7 @@
 <br>
 <div class="row footer">
    <footer class="py-3 bg-dark col-xs-12 col-sm-12 col-md-12 col-lg-12">
-       <p class ="m-0 text-center">Aluno: Henrique L. Hartmann, Turma: 3INFOi1</p>
+       <p class ="m-0 text-center">Aluno: André Luiz Pacheco da Costa, Turma: 3INFOi1</p>
    </footer>
 </div>
 </div>
